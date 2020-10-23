f8688ed3e10e920ad99f67a0215e3acf7aef3037
Noëlle Maccary <smaccary@student.42.fr>
2020-10-23 10:07:29 +0200

    yay

--- init.sh
+++ init.sh
@@ -7,3 +7,8 @@ sudo killall vsftpf
 sudo usermod -aG docker $(whoami);
 
 minikube start --vm-driver=docker
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
