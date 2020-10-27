d61248a34477b0a8bc7bb4a637a01403b29952e7
smaccary <smaccary@student.42.fr>
2020-10-27 13:07:27 +0100

    THE LOADBALANCER IS FUCKING WORKIGNG G G  G GGNG GN G GNGNG AAAAAAAAAAAHAHAHH AAH AHA AA

--- init.sh
+++ init.sh
@@ -10,6 +10,8 @@ sudo service docker restart
 
 minikube start --vm-driver=docker
 
+eval $(minikube docker-env)
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml > /dev/null
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml > /dev/null
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
